224e9d6c7542496edfc4387f8915809f89ef7bfd
Jordan Eizenga <joeizeng@gmail.com>
2017-07-22 07:56:44 +0200

    added union find

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,15 +1,349 @@
 //
 //  tests.cpp
 //
+// Unit tests for data structures in this repository
 //
 
 #include <stdio.h>
 #include <list>
+#include <vector>
 #include <iostream>
 #include <algorithm>
 #include <random>
 #include <cassert>
 
+vector<pair<size_t, size_t>> random_unions(size_t size) {
+    
+    int num_pairs = size * size;
+    
+    random_device rd;
+    default_random_engine gen(rd());
+    uniform_int_distribution<int> distr(0, num_pairs);
+    
+    vector<pair<size_t, size_t>> pairs;
+    vector<size_t> all_options(num_pairs);
+    
+    for (size_t i = 0; i < num_pairs; i++) {
+        all_options[i] = i;
+    }
+    
+    std::shuffle(all_options.begin(), all_options.end(), gen);
+    
+    int num_pairs_to_select = distr(gen);
+    
+    for (int i = 0; i < num_pairs_to_select; i++) {
+        pairs.emplace_back(all_options[i] / size, all_options[i] % size);
+    }
+    
+    return pairs;
+}
+
+void test_union_find_with_curated_examples() {
+    {
+        UnionFind union_find(10);
+        assert(union_find.find_group(0) != union_find.find_group(1));
+        
+        assert(union_find.group_size(0) == 1);
+        assert(union_find.group_size(1) == 1);
+        
+        union_find.union_groups(0, 1);
+        
+        assert(union_find.find_group(0) == union_find.find_group(1));
+        
+        assert(union_find.group_size(0) == 2);
+        assert(union_find.group_size(1) == 2);
+    }
+    
+    {
+        UnionFind union_find(10);
+        union_find.union_groups(0, 1);
+        union_find.union_groups(2, 3);
+        union_find.union_groups(3, 4);
+        union_find.union_groups(5, 6);
+        
+        assert(union_find.group_size(4) == 3);
+        assert(union_find.find_group(2) == union_find.find_group(3));
+        assert(union_find.find_group(5) == union_find.find_group(6));
+    }
+    
+    {
+        UnionFind union_find(10);
+        union_find.union_groups(0, 1);
+        union_find.union_groups(2, 3);
+        union_find.union_groups(3, 4);
+        union_find.union_groups(5, 6);
+        union_find.union_groups(2, 4);
+        
+        assert(union_find.group_size(4) == 3);
+        assert(union_find.find_group(2) == union_find.find_group(3));
+        assert(union_find.find_group(3) == union_find.find_group(4));
+    }
+    
+    {
+        UnionFind union_find(10);
+        union_find.union_groups(0, 1);
+        union_find.union_groups(2, 3);
+        union_find.union_groups(3, 4);
+        union_find.union_groups(5, 6);
+        union_find.union_groups(2, 4);
+        
+        vector<size_t> correct_group{2, 3, 4};
+        vector<size_t> group = union_find.group(3);
+        std::sort(group.begin(), group.end());
+        assert(group.size() == correct_group.size());
+        assert(std::equal(group.begin(), group.end(), correct_group.begin()));
+    }
+    
+    {
+        UnionFind union_find_1(10);
+        UnionFind union_find_2(10);
+        
+        union_find_1.union_groups(0, 1);
+        union_find_1.union_groups(2, 1);
+        union_find_1.union_groups(3, 2);
+        union_find_1.union_groups(4, 5);
+        union_find_1.union_groups(7, 6);
+        union_find_1.union_groups(7, 8);
+        union_find_1.union_groups(7, 9);
+        
+        union_find_2.union_groups(0, 1);
+        union_find_2.union_groups(2, 1);
+        union_find_2.union_groups(3, 2);
+        union_find_2.union_groups(4, 5);
+        union_find_2.union_groups(7, 6);
+        union_find_2.union_groups(7, 8);
+        union_find_2.union_groups(7, 9);
+        
+        vector<size_t> group_of_0_1 = union_find_1.group(0);
+        vector<size_t> group_of_4_1 = union_find_1.group(4);
+        vector<size_t> group_of_9_1 = union_find_1.group(9);
+        
+        vector<vector<size_t>> all_groups_1 = union_find_1.all_groups();
+        
+        vector<vector<size_t>> all_groups_2 = union_find_2.all_groups();
+        
+        vector<size_t> group_of_0_2 = union_find_2.group(0);
+        vector<size_t> group_of_4_2 = union_find_2.group(4);
+        vector<size_t> group_of_9_2 = union_find_2.group(9);
+        
+        vector<size_t> group_of_0_1_from_all, group_of_4_1_from_all, group_of_9_1_from_all;
+        vector<size_t> group_of_0_2_from_all, group_of_4_2_from_all, group_of_9_2_from_all;
+        
+        for (vector<size_t>& vec : all_groups_1) {
+            for (size_t i : vec) {
+                if (i == 0) {
+                    group_of_0_1_from_all = vec;
+                }
+                else if (i == 4) {
+                    group_of_4_1_from_all = vec;
+                }
+                else if (i == 9) {
+                    group_of_9_1_from_all = vec;
+                }
+            }
+        }
+        
+        for (vector<size_t>& vec : all_groups_2) {
+            for (size_t i : vec) {
+                if (i == 0) {
+                    group_of_0_2_from_all = vec;
+                }
+                else if (i == 4) {
+                    group_of_4_2_from_all = vec;
+                }
+                else if (i == 9) {
+                    group_of_9_2_from_all = vec;
+                }
+            }
+        }
+        
+        std::sort(group_of_0_1.begin(), group_of_0_1.end());
+        std::sort(group_of_4_1.begin(), group_of_4_1.end());
+        std::sort(group_of_9_1.begin(), group_of_9_1.end());
+        
+        std::sort(group_of_0_2.begin(), group_of_0_2.end());
+        std::sort(group_of_4_2.begin(), group_of_4_2.end());
+        std::sort(group_of_9_2.begin(), group_of_9_2.end());
+        
+        std::sort(group_of_0_1_from_all.begin(), group_of_0_1_from_all.end());
+        std::sort(group_of_4_1_from_all.begin(), group_of_4_1_from_all.end());
+        std::sort(group_of_9_1_from_all.begin(), group_of_9_1_from_all.end());
+        
+        std::sort(group_of_0_2_from_all.begin(), group_of_0_2_from_all.end());
+        std::sort(group_of_4_2_from_all.begin(), group_of_4_2_from_all.end());
+        std::sort(group_of_9_2_from_all.begin(), group_of_9_2_from_all.end());
+        
+        assert(group_of_0_1.size() == group_of_0_2.size());
+        assert(group_of_0_1.size() == group_of_0_1_from_all.size());
+        assert(group_of_0_1.size() == group_of_0_2_from_all.size());
+        
+        assert(group_of_4_1.size() == group_of_4_2.size());
+        assert(group_of_4_1.size() == group_of_4_1_from_all.size());
+        assert(group_of_4_1.size() == group_of_4_2_from_all.size());
+        
+        assert(group_of_9_1.size() == group_of_9_2.size());
+        assert(group_of_9_1.size() == group_of_9_1_from_all.size());
+        assert(group_of_9_1.size() == group_of_9_2_from_all.size());
+        
+        assert(std::equal(group_of_0_1.begin(), group_of_0_1.end(), group_of_0_1_from_all.begin()));
+        assert(std::equal(group_of_0_1.begin(), group_of_0_1.end(), group_of_0_2_from_all.begin()));
+        assert(std::equal(group_of_0_1.begin(), group_of_0_1.end(), group_of_0_2.begin()));
+        
+        assert(std::equal(group_of_4_1.begin(), group_of_4_1.end(), group_of_4_1_from_all.begin()));
+        assert(std::equal(group_of_4_1.begin(), group_of_4_1.end(), group_of_4_2_from_all.begin()));
+        assert(std::equal(group_of_4_1.begin(), group_of_4_1.end(), group_of_4_2.begin()));
+        
+        assert(std::equal(group_of_9_1.begin(), group_of_9_1.end(), group_of_9_1_from_all.begin()));
+        assert(std::equal(group_of_9_1.begin(), group_of_9_1.end(), group_of_9_2_from_all.begin()));
+        assert(std::equal(group_of_9_1.begin(), group_of_9_1.end(), group_of_9_2.begin()));
+    }
+    
+    {
+        UnionFind union_find_1(10);
+        UnionFind union_find_2(10);
+        
+        union_find_1.union_groups(0, 1);
+        union_find_1.union_groups(2, 1);
+        union_find_1.union_groups(3, 2);
+        union_find_1.union_groups(4, 5);
+        union_find_1.union_groups(7, 6);
+        union_find_1.union_groups(7, 8);
+        union_find_1.union_groups(7, 9);
+        
+        union_find_2.union_groups(0, 1);
+        union_find_2.union_groups(2, 1);
+        union_find_2.union_groups(3, 2);
+        union_find_2.union_groups(4, 5);
+        union_find_2.union_groups(7, 6);
+        union_find_2.union_groups(7, 8);
+        union_find_2.union_groups(7, 9);
+        
+        assert(union_find_1.group_size(0) == union_find_2.group(0).size());
+        assert(union_find_2.group_size(0) == union_find_1.group(0).size());
+        assert(union_find_1.group_size(0) == union_find_1.group(0).size());
+        assert(union_find_2.group_size(0) == union_find_2.group(0).size());
+        
+        assert(union_find_1.group_size(3) == union_find_2.group(3).size());
+        assert(union_find_2.group_size(3) == union_find_1.group(3).size());
+        assert(union_find_1.group_size(3) == union_find_1.group(3).size());
+        assert(union_find_2.group_size(3) == union_find_2.group(3).size());
+        
+        assert(union_find_1.group_size(9) == union_find_2.group(9).size());
+        assert(union_find_2.group_size(9) == union_find_1.group(9).size());
+        assert(union_find_1.group_size(9) == union_find_1.group(9).size());
+        assert(union_find_2.group_size(9) == union_find_2.group(9).size());
+    }
+    
+    cerr << "All curated UnionFind tests successful!" << endl;
+}
+
+void test_union_find_with_random_examples() {
+    for (size_t repetition = 0; repetition < 1000; repetition++) {
+        
+        UnionFind union_find(30);
+        
+        vector<pair<size_t, size_t>> unions = random_unions(union_find.size());
+        
+        vector<unordered_set<size_t>*> group_set_of(union_find.size());
+        vector<unordered_set<size_t>> group_sets(union_find.size());
+        for (size_t i = 0; i < union_find.size(); i++) {
+            group_set_of[i] = &group_sets[i];
+            group_sets[i].insert(i);
+        }
+        
+        for (pair<size_t, size_t> idxs : unions) {
+            union_find.union_groups(idxs.first, idxs.second);
+            
+            auto group_set_1 = group_set_of[idxs.first];
+            auto group_set_2 = group_set_of[idxs.second];
+            if (group_set_1 == group_set_2) {
+                continue;
+            }
+            for (size_t i : *group_set_2) {
+                group_set_1->insert(i);
+                group_set_of[i] = group_set_1;
+            }
+            group_set_2->clear();
+        }
+        
+        vector<vector<size_t>> groups_direct(union_find.size());
+        vector<vector<size_t>> groups_from_all(union_find.size());
+        vector<vector<size_t>> groups_orthogonal(union_find.size());
+        // alternate the order these operations are done in just in case the
+        // internal changes during find functions affects the outcome
+        if (repetition % 2 == 0) {
+            for (size_t i = 0; i < union_find.size(); i++) {
+                groups_direct[i] = union_find.group(i);
+            }
+            
+            vector<vector<size_t>> groups = union_find.all_groups();
+            for (vector<size_t>& group : groups) {
+                for (size_t i : group) {
+                    groups_from_all[i] = group;
+                }
+            }
+        }
+        else {
+            vector<vector<size_t>> groups = union_find.all_groups();
+            for (vector<size_t>& group : groups) {
+                for (size_t i : group) {
+                    groups_from_all[i] = group;
+                }
+            }
+            
+            for (size_t i = 0; i < union_find.size(); i++) {
+                groups_direct[i] = union_find.group(i);
+            }
+        }
+        
+        for (size_t i = 0; i < union_find.size(); i++) {
+            vector<size_t>& group = groups_orthogonal[i];
+            for (size_t j : *group_set_of[i]) {
+                group.push_back(j);
+            }
+        }
+        
+        for (size_t i = 0; i < union_find.size(); i++) {
+            
+            std::sort(groups_orthogonal[i].begin(), groups_orthogonal[i].end());
+            std::sort(groups_from_all[i].begin(), groups_from_all[i].end());
+            std::sort(groups_direct[i].begin(), groups_direct[i].end());
+            
+            
+            assert(union_find.group_size(i) == groups_from_all[i].size());
+            assert(union_find.group_size(i) == groups_direct[i].size());
+            assert(groups_from_all[i].size() == groups_direct[i].size());
+            assert(groups_orthogonal[i].size() == groups_direct[i].size());
+            
+            if (!std::equal(groups_from_all[i].begin(), groups_from_all[i].end(),
+                            groups_direct[i].begin())) {
+                cerr << "FAILURE mismatch in repetition " << repetition << " in the following groups containing " << i << endl;
+                cerr << "computed directly: ";
+                for (size_t j : groups_direct[i]) {
+                    cerr << j << " ";
+                }
+                cerr << endl;
+                cerr << "computed in batch: ";
+                for (size_t j : groups_from_all[i]) {
+                    cerr << j << " ";
+                }
+                cerr << endl;
+                cerr << "groups formed by unions: ";
+                for (pair<size_t, size_t> idxs : unions) {
+                    cerr << "(" << idxs.first << "," << idxs.second << ") ";
+                }
+                cerr << endl;
+            }
+            
+            assert(std::equal(groups_from_all[i].begin(), groups_from_all[i].end(),
+                               groups_direct[i].begin()));
+            assert(std::equal(groups_from_all[i].begin(), groups_from_all[i].end(),
+                               groups_orthogonal[i].begin()));
+        }
+    }
+    
+    cerr << "All randomized UnionFind tests successful!" << endl;
+}
 
 void check_heap_invariants(MinMaxHeap<int>& heap, list<int>& vals) {
     
@@ -118,4 +452,6 @@ void test_min_max_heap() {
 int main(void) {
     
     test_min_max_heap();
+    test_union_find_with_curated_examples();
+    test_union_find_with_random_examples();
 }
---
+++ src/union_find.hpp
@@ -0,0 +1,169 @@
+//
+//  union_find.hpp
+//
+// Contains an implementation of a custom union-find with some additional capabilities
+//
+
+#ifndef union_find_hpp
+#define union_find_hpp
+
+#include <vector>
+#include <unordered_set>
+
+/**
+ * A custom Union-Find data structure that supports merging a set of indices in
+ * disjoint sets in amortized nearly linear time. This implementation also supports
+ * querying the size of the group containing an index in constant time and querying
+ * the members of the group containing an index in linear time in the size of the group.
+ */
+class UnionFind {
+public:
+    /// Construct UnionFind for this many indices
+    UnionFind(size_t size);
+    
+    /// Destructor
+    ~UnionFind();
+    
+    /// Returns the number of indices in the UnionFind
+    size_t size();
+    
+    /// Returns the group ID that index i belongs to (can change after calling union)
+    size_t find_group(size_t i);
+    
+    /// Merges the group containing index i with the group containing index j
+    void union_groups(size_t i, size_t j);
+    
+    /// Returns the size of the group containing index i
+    size_t group_size(size_t i);
+    
+    /// Returns a vector of the indices in the same group as index i
+    vector<size_t> group(size_t i);
+    
+    /// Returns all of the groups, each in a separate vector
+    vector<vector<size_t>> all_groups();
+    
+private:
+    
+    struct UFNode;
+    vector<UFNode> uf_nodes;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+struct UnionFind::UFNode {
+    UFNode(size_t index) : head(index), rank(0), size(1) {}
+    ~UFNode() {}
+    
+    size_t rank;
+    size_t size;
+    size_t head;
+    unordered_set<size_t> children;
+};
+
+UnionFind::UnionFind(size_t size) {
+    uf_nodes.reserve(size);
+    for (size_t i = 0; i < size; i++) {
+        uf_nodes.emplace_back(i);
+    }
+}
+
+UnionFind::~UnionFind() {
+    // nothing to do
+}
+
+size_t UnionFind::size() {
+    return uf_nodes.size();
+}
+
+size_t UnionFind::find_group(size_t i) {
+    vector<size_t> path;
+    // traverse tree upwards
+    while (uf_nodes[i].head != i) {
+        path.push_back(i);
+        i = uf_nodes[i].head;
+    }
+    // compress path
+    unordered_set<size_t>& head_children = uf_nodes[i].children;
+    for (size_t p = 1; p < path.size(); p++) {
+        size_t j = path[p - 1];
+        uf_nodes[j].head = i;
+        uf_nodes[path[p]].children.erase(j);
+        head_children.insert(j);
+    }
+    // note: don't need to compress path for the final index since it
+    // already points to the head
+    return i;
+}
+
+void UnionFind::union_groups(size_t i, size_t j) {
+    size_t head_i = find_group(i);
+    size_t head_j = find_group(j);
+    if (head_i == head_j) {
+        // the indices are already in the same group
+        return;
+    }
+    else {
+        // use rank as a pivot to determine which group to make the head
+        UFNode& node_i = uf_nodes[head_i];
+        UFNode& node_j = uf_nodes[head_j];
+        if (node_i.rank > node_j.rank) {
+            node_j.head = head_i;
+            node_i.children.insert(head_j);
+            node_i.size += node_j.size;
+        }
+        else {
+            node_i.head = head_j;
+            node_j.children.insert(head_i);
+            node_j.size += node_i.size;
+            
+            if (node_j.rank == node_i.rank) {
+                node_j.rank++;
+            }
+        }
+    }
+}
+
+size_t UnionFind::group_size(size_t i) {
+    return uf_nodes[find_group(i)].size;
+}
+
+vector<size_t> UnionFind::group(size_t i) {
+    vector<size_t> to_return;
+    // go to head of group
+    vector<size_t> stack{find_group(i)};
+    // traverse tree downwards to find all indices in group
+    while (!stack.empty()) {
+        size_t curr = stack.back();
+        stack.pop_back();
+        to_return.push_back(curr);
+        unordered_set<size_t>& children = uf_nodes[curr].children;
+        for (size_t child : children) {
+            stack.push_back(child);
+        }
+    }
+    return to_return;
+}
+
+vector<vector<size_t>> UnionFind::all_groups() {
+    vector<vector<size_t>> to_return(uf_nodes.size());
+    for (size_t i = 0; i < uf_nodes.size(); i++) {
+        to_return[find_group(i)].push_back(i);
+    }
+    auto new_end = std::remove_if(to_return.begin(), to_return.end(),
+                                  [](const vector<size_t>& grp) { return grp.empty(); });
+    to_return.resize(new_end - to_return.begin());
+    return to_return;
+}
+
+
+
+#endif /* union_find_hpp */
