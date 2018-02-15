7bb3e9914659921f1abf71f911adf61542887b22
Jordan Eizenga <joeizeng@gmail.com>
2018-02-15 11:13:49 -0800

    switched to apache 2.0 license

--- src/min_max_heap.hpp
+++ src/min_max_heap.hpp
@@ -1,16 +1,21 @@
-//This program is free software: you can redistribute it and/or modify
-//it under the terms of the GNU General Public License as published by
-//the Free Software Foundation, either version 3 of the License, or
-//(at your option) any later version.
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+//                                               "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// http://www.apache.org/licenses/LICENSE-2.0
 //
-//You should have received a copy of the GNU General Public License
-//along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+
 //  min_max_heap.hpp
 //  
 // Contains a template implementation of a min-max heap
@@ -69,9 +74,7 @@ private:
     inline void post_add();
     void restore_heap_below(size_t i, int level);
     void restore_heap_above(size_t i, int level);
-    inline static bool cmp(const T& v1, const T& v2, int level) {
-        return (level % 2 == 0) != (v1 > v2);
-    }
+    inline static bool cmp(const T& v1, const T& v2, int level);
     
     vector<T> values;
 };
@@ -130,6 +133,11 @@ MinMaxHeap<T>::MinMaxHeap(Iter begin, Iter end) {
 }
 
 template <typename T>
+inline static bool MinMaxHeap<T>::cmp(const T& v1, const T& v2, int level) {
+    return (level % 2 == 0) != (v1 > v2);
+}
+
+template <typename T>
 void MinMaxHeap<T>::restore_heap_below(size_t i, int level) {
     // the range of i's grandchildren
     size_t rightest = 4 * i + 6;
@@ -174,7 +182,7 @@ void MinMaxHeap<T>::restore_heap_below(size_t i, int level) {
             }
         }
         
-        // if we swapped, and not with child (because then it has no grand children and
+        // if we swapped, and not with child (because then it has nogrand children and
         // invariant is vacuously maintained below) then recurse downward
         if (!direct_child_swapped && most != i) {
             swap(values[i], values[most]);
